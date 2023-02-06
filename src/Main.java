0a7036357b5646326796a6f59202587e149e103b
Dikontay <118036214+Dikontay@users.noreply.github.com>
2023-02-06 18:09:24 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello aitu!");
+        System.out.println("Hello AEED, let's start!!!");
+        
     }
 }
